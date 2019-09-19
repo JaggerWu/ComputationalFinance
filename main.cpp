bd55764efc28ddf9be00304af60af3a465e539f3
Jian Wu <xcb479@alumni.ku.dk>
2019-09-19 22:49:16 +0200

    try MT random

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<stdio.h>
+#include<random>
 
 #include "Common/Math.h"
 #include "Common/Gaussian.h"
 #include "Option/PayOff.h"
-
-using namespace std;
 
 int main()
 {
@@ -24,26 +23,35 @@ int main()
     printf ("The complex number is (%f, %f) \n", tt->re, tt->im);
     delete tt;
 
-    cout << (a == b) << endl;
+    std::cout << (a == b) << std::endl;
 
     printf ("The complex number is (%f, %f) \n", d.re, d.im);
     printf ("The complex number is (%f, %f) \n", a.re, a.im);
 
     PayOffCall call_(1.20);
 
-    cout << "The strike is" << 1.23 << endl;
-    cout << "The pay off is " << call_(1.21) << endl;
+    std::cout << "The strike is" << 1.23 << std::endl;
+    std::cout << "The pay off is " << call_(1.21) << std::endl;
 
     PayOffDoubleDigital test(1.50, 1.30);
-    cout << "The pay off double digital is " << test(1.21) << endl;
+    std::cout << "The pay off double digital is " << test(1.21) << std::endl;
 
     PayOffDoubleDigital test2(DigitalType::lowerLevel, 1.1);
     PayOff* test3 = test2.clone();
 
-    cout << "The pay off digital(test2) is " << test2(1.01) << endl;
-    cout << "The pay off digital(test3) is " << test3->operator()(1.01) << endl;
+    std::cout << "The pay off digital(test2) is " << test2(1.01) << std::endl;
+    std::cout << "The pay off digital(test3) is " << test3->operator()(1.01) << std::endl;
 
-    cout << "Get gaussian is " << getOneGaussianByCLT()<< endl;
+    std::cout << "Get gaussian is " << getOneGaussianByCLT()<< std::endl;
+
+    std::mt19937 generator (1);
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
+
+    double randomRealBetweenZeroAndOne = dis(generator);
+    double randomRealBetweenZeroAndOne2 = dis(generator);
+
+    std::cout << "The random number is " << randomRealBetweenZeroAndOne << std::endl;
+    std::cout << "The random number is " << randomRealBetweenZeroAndOne2 << std::endl; 
     
     return EXIT_SUCCESS;
 }
